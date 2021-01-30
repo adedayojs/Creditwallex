977e568f03dd610fdb1bfda5aa256e32bcc7fffe
adedayojs <aadedunye@gidimobile.com>
2021-01-30 22:58:05 +0100

    (Update): Added fontawesome for icon and shared module to aid reusablity

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [CoreComponent],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    SharedModule
   ]
 })
 export class CoreModule { }
