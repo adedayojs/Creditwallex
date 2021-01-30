c00c4987c52344cf59036657cb31c020786e9c1c
adedayojs <aadedunye@gidimobile.com>
2021-01-30 23:47:19 +0100

    (Feature): Finished root component

--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { faCommentAlt, faBell } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-core',
   templateUrl: './core.component.html',
-  styleUrls: ['./core.component.scss']
+  styleUrls: ['./core.component.scss'],
 })
 export class CoreComponent implements OnInit {
-navItems = [
-  {name:'Dashboard', url:'/assets/Dashboard/dashboard.svg', link:'dashboard'},
-  {name:'Starships', url:'/assets/Dashboard/building.svg', link:'starships'},
-  {name:'People', url:'/assets/Dashboard/shopping-basket (1).svg', link:'people'},
-  {name:'Vehicles', url:'/assets/Dashboard/menu.svg', link:'vehicles'},
-  {name:'Species', url:'/assets/Dashboard/menu.svg', link:'species'},
-
-]
-  constructor() { }
-
+  navItems = [
+    {
+      name: 'Dashboard',
+      url: '/assets/Dashboard/dashboard.svg',
+      link: 'dashboard',
+    },
+    {
+      name: 'Starships',
+      url: '/assets/Dashboard/building.svg',
+      link: 'starships',
+    },
+    {
+      name: 'People',
+      url: '/assets/Dashboard/shopping-basket (1).svg',
+      link: 'people',
+    },
+    { name: 'Vehicles', url: '/assets/Dashboard/menu.svg', link: 'vehicles' },
+    { name: 'Species', url: '/assets/Dashboard/menu.svg', link: 'species' },
+  ];
+  icons = {
+    comment: faCommentAlt,
+    bell: faBell,
+  };
+  bell = faBell;
+  constructor(private route: ActivatedRoute) {}
+  pageTitle: string;
   ngOnInit(): void {
+    console.log(this.route);
+    this.route.url.subscribe((res) => {});
   }
-
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import {
+  FontAwesomeModule,
+  FaIconLibrary,
+} from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 @NgModule({
   declarations: [],
   imports: [CommonModule, FontAwesomeModule],
   exports: [FontAwesomeModule],
 })
-export class SharedModule {}
+export class SharedModule {
+  constructor(library: FaIconLibrary) {
+    // Add an icon to the library for convenient access in other components
+    library.addIconPacks(fas, far, fab);
+  }
+}
