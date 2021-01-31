90156378f67964830b262e768d421b74e128c118
adedayojs <aadedunye@gidimobile.com>
2021-01-31 04:56:57 +0100

    (Feature): Completed Implementation of Dropdown on Dashboard page

--- src/app/core/modules/dashboard/dashboard.component.ts
+++ src/app/core/modules/dashboard/dashboard.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  constructor() {}
+  years = [
+    { item: 'Current Year' },
+    { item: 'Last Year' },
+    { item: '2018' },
+    { item: '2017' },
+  ];
+  selectedYear = this.years[0].item;
+  weeks = [
+    { item: 'Current Week' },
+    { item: 'Last Week' },
+    { item: '2018' },
+    { item: '2017' },
+  ];
+  selectedWeek = this.weeks[0].item;
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  updateChanges(category: 'year' | 'week', data: { item: string }) {
+    console.log();
+    category === 'year'
+      ? (this.selectedYear = data.item)
+      : (this.selectedWeek = data.item);
   }
-
 }
