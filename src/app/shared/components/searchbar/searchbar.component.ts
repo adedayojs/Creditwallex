9bc9f42df6fe05fd2924114bf29ccd401524874c
adedayojs <aadedunye@gidimobile.com>
2021-02-15 07:33:57 +0100

    Emit data when input field changes

--- src/app/shared/components/searchbar/searchbar.component.ts
+++ src/app/shared/components/searchbar/searchbar.component.ts
@@ -19,6 +19,8 @@ export class SearchbarComponent implements OnInit {
   @Output() input = new EventEmitter();
   constructor(private location: Location) {}
 
+  /* Helper Variables */
+  searchInput
   ngOnInit(): void {}
 
   /* Navigator button */
@@ -29,5 +31,10 @@ export class SearchbarComponent implements OnInit {
   /* Pagination handler */
   paginate(action: 'prev' | 'next') {
     action === 'prev' ? this.prevPage.emit(action) : this.nextPage.emit(action);
+  }
+  /* Search Handler */
+  search(){
+    console.log(this.searchInput)
+    this.input.emit(this.searchInput)
   }
 }
